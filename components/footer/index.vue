b0790d4526c209443c16aab6ed9da0ec159b9e7a
Davi Batista <davideosmar11@gmail.com>
2023-10-19 19:35:04 -0300

    components(cards): added icons to shop cards

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -10,7 +10,7 @@
             linksDiv: "flex flex-row gap-14",
             socialLinks: "flex flex-row gap-14 justify-center",
             span: "flex flex-row gap-0",
-            input: "bg-zinc-200 w-72",
+            input: "bg-zinc-200 w-72 p-2 px-4 text-zinc-600",
             button: "h-12 w-12 bg-red-600 grid place-content-center",
             h5: "font-bold text-3xl font-ms"
         },
@@ -27,7 +27,7 @@
                 <h5 :class="styles.top.h5">Join Daniels' News!</h5>
                 <p>Get news, discounts, recipes and more!</p>
                 <span :class="styles.top.span">
-                    <input type="text" :class="styles.top.input">
+                    <input type="text" :class="styles.top.input" placeholder="Your e-mail here...">
                     <button :class="styles.top.button">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="w-8 h-8"  viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
--- components/store_card/index.vue
+++ components/store_card/index.vue
@@ -4,9 +4,11 @@
     const styles = {
         card: "flex flex-col bg-zinc-100 h-[270px] items-left w-[250px]",
         name: "text-slate-900 font-ms text-lg font-semibold",
-        address: "pt-1 h-[35px]",
-        phone: "font-ms text-xs",
-        texts: "p-2 text-xs font-pp"
+        address: "pt-1",
+        phone: "font-ms text-sm",
+        texts: "p-2 text-xs font-pp",
+        span: "flex flex-row my-1 items-center",
+        icons: "w-3 h-3 mr-2"
     }
     
 </script>
@@ -16,8 +18,18 @@
         <img :src="store.imgUrl">
         <div :class="styles.texts">
             <h6 :class="styles.name">{{store.name}}</h6>
-            <p :class="styles.address">{{store.address}}</p>
-            <p :class="styles.phone">{{store.phone}}</p>
+            <span :class="styles.span">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" :class="styles.icons" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+                    <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+                </svg>
+                <p :class="styles.address">{{store.address}}</p>
+            </span>
+            <span :class="styles.span">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" :class="styles.icons" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+                </svg>
+                <p :class="styles.phone">{{store.phone}}</p>
+            </span>
         </div>
     </div>
 </template>
