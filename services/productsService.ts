1d74d87d834a1b1e3061d98c87a83d22fa123e5b
Davi Batista <davideosmar11@gmail.com>
2024-01-06 18:55:33 -0300

    services(product): added basic product services

--- services/productsService.ts
+++ services/productsService.ts
@@ -1,17 +1,54 @@
 import api from "./api";
 
+export type ProductType = {
+    id: string,
+    title: string,
+    price: number,
+    imgUrl: string,
+    description: string,
+    category: string,
+    featured: boolean
+}
+
 const productsService = {
     /* Get all products */
-    findAll: () => api.get("/products"),
+    findAll: async () => {
+        const response = api.get("/products/get").catch((err) => {
+            return err.response
+        })
+
+        return response
+    },
 
     /* Get one product by its id */
-    findOne: (id: string) => api.get(`/products/${id}`),
+    findOne: async (productId: string) => {
+        const response = api.get(`/products/get/${productId}`).catch((err) => {
+            return err.response
+        })
+
+        return response
+    },
 
     /* Get all the products of a specific category by its id */
-    findByCategory: (id: string) => api.get(`/products/category/${id}`),
+    findByCategory: async (categoryId: string) => {
+        const response = api.get(`/products/getByCategory/${categoryId}`).catch((err) => {
+            return err.response
+        })
+
+        return response
+    },
 
     /* Search a product by its name */
-    search: (name: string) => api.get(`/products/search/${name}`),
+    search: async (name: string) => {
+        const response = api.get(`/products/search?title=${name}`).catch((err) => {
+            return err.response
+        })
+
+        return response
+    },
+
+    /* Create a new product */
+    create: (data: any) => api.post("/products/create", data),
 }
 
 export default productsService
