bad61d5e4e32db3ef0c7b4e3848eb395e271bd57
Davi Batista <davideosmar11@gmail.com>
2024-01-06 19:30:55 -0300

    services(auth): added user register request

--- services/authService.ts
+++ services/authService.ts
@@ -1,15 +1,33 @@
 import api from "./api"
 
+interface RegisterParams {
+    name: string;
+    email: string;
+    password: string;
+}
+
 interface LoginParams {
     email: string;
     password: string;
 }
 
 const authService   =   {
+    /* User register */
+    register: async (params: RegisterParams) => {
+        const response = await api.post("/auth/register", params).catch((error) => {
+            if (error.response.status === 400 || error.response.status === 401) {
+                return error.response;
+            }
+
+            return error;
+        })
+
+        return response;
+    },
+
     /* User login */
     login: async (params: LoginParams) => {
-
-        const res = await api.post("/auth/login", params).catch((error) => {
+        const response = await api.post("/auth/login", params).catch((error) => {
 
         if (error.response.status === 400 || error.response.status === 401) {
             return error.response;
@@ -20,11 +38,11 @@ const authService   =   {
 
         })
 
-        if (res.status === 200) {
-            sessionStorage.setItem("token", res.data.token);
+        if (response.status === 200) {
+            sessionStorage.setItem("token", response.data.token);
         }
 
-        return res;
+        return response;
     }
 }
 
