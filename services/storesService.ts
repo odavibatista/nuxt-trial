193ea29cd96540568cf9197a0d09bfb915c422d9
Davi Batista <davideosmar11@gmail.com>
2024-01-27 15:00:42 -0300

    services(stores): changed store services

--- services/storesService.ts
+++ services/storesService.ts
@@ -21,13 +21,74 @@ const storesService = {
     /* Get all stores */
     findAll: async () => {
         const { data: response } = await apiRequest(`/stores/get`, {
-            method: 'GET'
+            method: 'GET',
+            headers: {
+                "client-platform": "browser",
+            }
         }).catch((err) => {
             return err.response
         })
 
         return toRaw(response.value)
     },
+
+    /* Get one store by its id */
+    getById: async (storeId: string) => {
+        const { data: response } = await apiRequest(`/stores/get/${storeId}`, {
+            method: 'GET',
+            headers: {
+                "client-platform": "browser",
+            }
+        }).catch((err) => {
+            return err.response
+        })
+
+        return toRaw(response.value)
+    },
+
+    /* Create a new store */
+    create: async (store: CreateStoreParams) => {
+        const { data: response } = await apiRequest(`/stores/create`, {
+            method: 'POST',
+            headers: {
+                "client-platform": "browser",
+            },
+            body: store
+        }).catch((err) => {
+            return err.response
+        })
+
+        return toRaw(response.value)
+    },
+
+    /* Update a store */
+    update: async (store: StoreType) => {
+        const { data: response } = await apiRequest(`/stores/update/${store.id}`, {
+            method: 'PUT',
+            headers: {
+                "client-platform": "browser",
+            },
+            body: store
+        }).catch((err) => {
+            return err.response
+        })
+
+        return toRaw(response.value)
+    },
+
+    /* Delete a store */
+    delete: async (storeId: string) => {
+        const { data: response } = await apiRequest(`/stores/delete/${storeId}`, {
+            method: 'DELETE',
+            headers: {
+                "client-platform": "browser",
+            }
+        }).catch((err) => {
+            return err.response
+        })
+
+        return toRaw(response.value)
+    }
 }
 
 export default storesService
